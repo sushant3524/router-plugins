fa8535c79c738a74de2be155546ea92fadd5a255
Soumik Dutta <shalearkane@gmail.com>
2024-07-02 16:30:17 +0530

    upgrade dependencies and fix the Rust compiler to fix release builds

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 set -e
 
-echo "Starting Helper"
-nohup /dist/helper &
-
+# start Router in background first since Router can sometimes break but that will not stop the deployment
+# useful for debug environments where we will want to manually test and debug the router
 echo "Starting Router"
-nohup /dist/router --hr
+nohup /dist/router --hr &
+
+echo "Starting Helper"
+nohup /dist/helper
